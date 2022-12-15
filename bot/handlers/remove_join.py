21ea218b2bbc8d9dd3c007d0273b75c1d9780b5a
bekhruz <yoshlikmedia@gmail.com>
2022-12-15 18:14:05 +0300

    textga biroz o'zgartirishlar kiritildi

--- bot/handlers/remove_join.py
+++ bot/handlers/remove_join.py
@@ -2,16 +2,13 @@ from aiogram import F, Router
 from aiogram.types import ContentType, Message
 
 router = Router()
-router.message.filter(F.chat.type == "group")
 
 
 @router.message(
-    ~F.content_type(ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER)
+    F.content_type.in_({ContentType.NEW_CHAT_MEMBERS, ContentType.LEFT_CHAT_MEMBER})
 )
 async def remove_join(message: Message):
     await message.delete()
     if message.content_type == ContentType.NEW_CHAT_MEMBERS:
-        text = "Assalomu alaykum, {}! aiogram[uz] kommunityga xush kelibsiz!".format(
-            message.from_user.full_name
-        )
+        text = f"Assalomu alaykum {message.from_user.mention_html()}, aiogram[uz] kommunityga xush kelibsiz!"
         await message.answer(text)
