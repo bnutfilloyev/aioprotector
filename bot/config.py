891808d8e4bbc76b1bfa8feed8bcbe4e0a73a450
bekhruz <yoshlikmedia@gmail.com>
2022-12-15 15:44:02 +0300

    init commits

---
+++ bot/config.py
@@ -0,0 +1,16 @@
+from typing import Optional
+
+from pydantic import BaseSettings, SecretStr
+
+
+class Settings(BaseSettings):
+    bot_token: SecretStr
+    admins: int
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+        env_nested_delimiter = '__'
+
+
+config = Settings()
