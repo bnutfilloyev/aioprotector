ad52d25369b567493b682a4d8efeea975250baf6
bekhruz <yoshlikmedia@gmail.com>
2022-12-15 17:39:39 +0300

    Faqat asosiy config parametrlari saqlandi

--- bot/config.py
+++ bot/config.py
@@ -8,9 +8,9 @@ class Settings(BaseSettings):
     admins: int
 
     class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
-        env_nested_delimiter = '__'
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        env_nested_delimiter = "__"
 
 
 config = Settings()
